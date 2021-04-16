c6f321648eb48edf4a7d0aa039ed629d2296c780
Lan Nhi <33212571+lannhi2208b@users.noreply.github.com>
2021-04-16 17:17:00 +0700

    Update

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Native } from '../NativeDemo/App/index';
-import { AppRegistry } from 'react-native';
 
 export default function App() {
     return (
@@ -12,4 +10,3 @@ export default function App() {
     );
 };
 
-// AppRegistry.registerComponent('main',() => App);
--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -1,17 +1,3 @@
 
 // Simple React Native specific changes
 const URI = 'http://192.168.92.2:8000';
-
-export default {
-    async fetchConfig() {
-        try {
-            let response = fetch('http://192.168.92.2:8000/api/register').then((response) => response.json());
-            //let responseJsonData = response.json();
-            console.log(response);
-            return response;
-        }
-        catch(e) {
-            console.log(e)
-        }
-    }
-}
